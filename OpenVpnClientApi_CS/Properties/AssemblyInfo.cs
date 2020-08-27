bad242b0b21336f7377db6ec1f33ce6250c94228
Michael Vanantwerp <michaelv275@gmail.com>
2020-08-27 15:38:31 -0500

    Initial package commit

--- OpenVpnClientApi_CS/Properties/AssemblyInfo.cs
+++ OpenVpnClientApi_CS/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OpenVpnClientApi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A C# wrapper for the OpenVPN3 library client API.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Dispel")]
 [assembly: AssemblyProduct("OpenVpnClientApi")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]

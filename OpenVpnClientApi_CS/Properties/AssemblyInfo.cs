1a89ddca2bcfd9957adf9ed95e682e81bd315ae4
Michael Vanantwerp <61846223+michaelv275@users.noreply.github.com>
2025-01-30 18:00:56 -0600

    Working import with nuget

--- OpenVpnClientApi_CS/Properties/AssemblyInfo.cs
+++ OpenVpnClientApi_CS/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OpenVpnClientApi")]
+[assembly: AssemblyTitle("OpenVpnClientApi_CS")]
 [assembly: AssemblyDescription("A C# wrapper for the OpenVPN3 library client API.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dispel")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.5")]
-[assembly: AssemblyFileVersion("2.0.0.5")]
+[assembly: AssemblyVersion("2.0.0.1")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
